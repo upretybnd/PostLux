fe76a8c65443526e73e52b1e99b0188003899234
amritregmi26 <amrregmi@gmail.com>
2023-10-23 16:25:42 +0545

    Routing for Home, Login and Signup page has been updated

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Signup from './pages/Auth/signup';
+import Home from './pages/Home/main';
+import Login from './pages/Auth/login';
+
+const App = () => {
+    return(
+        <BrowserRouter>
+            <Routes>
+                <Route path='/' exact element = {<Signup />} />
+                <Route path='/login' element = {<Login />} />
+                <Route path='/home' element = {<Home />} />
+            </Routes>
+        </BrowserRouter>
+    );
+}
+
+export default App;
